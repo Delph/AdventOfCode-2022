864713f2fffebe93cb0965105f46bd3537f4e58b
Delph <49454418+Delph@users.noreply.github.com>
2022-12-04 09:21:16 +0100

    day4

---
+++ day4/p1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+with open('input.txt') as f:
+  input = f.read().splitlines()
+
+
+overlap = 0
+for pair in input:
+  a, b = pair.split(',')
+  al, ah = map(int, a.split('-'))
+  bl, bh = map(int, b.split('-'))
+
+  if al <= bl and ah >= bh or bl <= al and bh >= ah:
+    overlap += 1
+print(overlap)
